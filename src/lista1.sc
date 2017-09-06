37ecd489ef7b09ba5d65c9fb9b23fe2f201b92f4
Fabio Mascarenhas <mascarenhas@gmail.com>
2017-09-06 11:53:50 -0300

    gabarito

--- src/lista1.sc
+++ src/lista1.sc
@@ -1,1 +1,55 @@
 import lista1._
+
+quicksort(List(3, 3, 3))
+quicksortH(List(3, 3, 3))
+
+val sem1 = List((9.5,3),(7.3,4),(5.0,3),(4.0,4))
+
+(9.5 * 3 + 7.3 * 4 + 5 * 3 + 4 * 4) /
+  (3 + 4 + 3 + 4)
+
+crSemestre(sem1)
+
+crsAcumulados(List(sem1, sem1, sem1))
+
+contem(unitario(2), 3)
+contem(unitario(2), 2)
+
+contem(uniao(unitario(2),
+  unitario(3)), 3)
+
+contem(uniao(uniao(uniao(unitario(2),
+  unitario(3)), unitario(4)),
+  unitario(5)), 6)
+
+val pares: Conjunto[Int] = (x: Int) => x % 2 == 0
+
+contem(pares, 4)
+contem(pares, 7)
+
+val quadrados: Conjunto[Int] = (x: Int) => Math.sqrt(x).round == Math.sqrt(x)
+
+val paresQuadrados: Conjunto[Int] =
+  intersecao(pares, quadrados)
+
+contem(paresQuadrados, 4)
+contem(paresQuadrados, 20)
+contem(paresQuadrados, 9)
+contem(pares, 20)
+contem(quadrados, 9)
+
+val paresStr: Conjunto[String] = map(pares,
+  (s: String) => s.toInt)
+
+contem(paresStr, "4")
+contem(paresStr, "9")
+
+for {
+  x <- ConjVazio().insere(2).insere(1).insere(5)
+  y <- ConjVazio().insere(4).insere(3)
+} yield x + y
+
+val x = ConjVazio().insere(2).insere(4).insere(5)
+val y = ConjVazio().insere(4).insere(3)
+
+intersecao(x, y)
--- src/lista1.scala
+++ src/lista1.scala
@@ -1,38 +1,131 @@
 package object lista1  {
   // defina as funções da sua resposta aqui
 
-  def pascal(col: Int, lin: Int): Int = ???
+  def pascal(col: Int, lin: Int): Int =
+    if (col == 0 || col == lin)
+      1
+    else
+      pascal(col, lin - 1) + pascal(col - 1, lin - 1)
 
-  def balanceado(l: List[Char]): Boolean = ???
+  def balanceado(l: List[Char]): Boolean = {
+    def loop(l: List[Char], nivel: Int): Boolean = l match {
+      case '(' :: t => loop(t, nivel+1)
+      case ')' :: t => if (nivel == 0)
+        false else loop(t, nivel-1)
+      case h :: t => loop(t, nivel)
+      case List() => nivel == 0
+    }
+    loop(l, 0)
+  }
 
-  def particao(l: List[Int], pivo: Int): (List[Int], List[Int]) = ???
-  def quicksort(l: List[Int]): List[Int] = ???
+  def particao(l: List[Int], pivo: Int): (List[Int], List[Int]) = {
+    def loop(l: List[Int], r: (List[Int], List[Int]), pivo: Int):
+        (List[Int], List[Int]) = (l, r) match {
+      case (h :: t, (l1, l2)) => if (h <= pivo)
+          loop(t, (h :: l1, l2), pivo)
+        else
+          loop(t, (l1, h :: l2), pivo)
+      case (List(), r) => r
+    }
+    loop(l, (List(), List()), pivo)
+  }
+  def quicksort(l: List[Int]): List[Int] = l match {
+    case h :: t => {
+      val (l1, l2) = particao(t, h)
+      quicksort(l1) ++ List(h) ++ quicksort(l2)
+    }
+    case List() => List()
+  }
 
-  def crSemestre(notas: List[(Double, Int)]): (Double, Int) = ???
-  def crsAcumulados(semestres: List[List[(Double, Int)]]): (List[Double], Int) = ???
+  def quicksortH(l: List[Int]): List[Int] = l match {
+    case h :: t =>
+      quicksortH(t.filter(x => x <= h)) ++ List(h) ++
+        quicksortH(t.filter(x => x > h))
+    case List() => List()
+  }
+
+  def crSemestre(notas: List[(Double, Int)]): (Double, Int) =
+    notas.reduceLeft((acum, materia) => {
+      val (cra, creditosa) = acum
+      val (nota, credito) = materia
+      ((cra * creditosa + nota * credito) / (creditosa + credito),
+        creditosa + credito)
+    })
+
+  def crsAcumulados(semestres: List[List[(Double, Int)]]):
+    (List[Double], Int) = {
+    val acumSemestres: List[(Double, Int)] = semestres.map(crSemestre)
+    acumSemestres.foldLeft((List[Double](), 0))((acum, semestre) => {
+      val (cras, creditos) = acum
+      val (craSem, creditosSem) = semestre
+      (cras ++ List((cras.lastOption.getOrElse(0.0) *
+        creditos + craSem * creditosSem) /
+        (creditos + creditosSem)), creditos + creditosSem)
+    })
+  }
 
   type Conjunto[T] = T => Boolean
 
   def contem[T](conj: Conjunto[T], elem: T) = conj(elem)
-  def unitario[T](elem: T): Conjunto[T] = ???
-  def uniao[T](c1: Conjunto[T], c2: Conjunto[T]): Conjunto[T] = ???
-  def intersecao[T](c1: Conjunto[T], c2: Conjunto[T]): Conjunto[T] = ???
-  def diferenca[T](c1: Conjunto[T], c2: Conjunto[T]): Conjunto[T] = ???
-  def filtro[T](c: Conjunto[T], f: T => Boolean): Conjunto[T] = ???
-  def map[T, U](c: Conjunto[T], f: U => T): Conjunto[U] = ???
+  def unitario[T](elem: T): Conjunto[T] = (x: T) => x == elem
+  def uniao[T](c1: Conjunto[T], c2: Conjunto[T]): Conjunto[T] =
+    (x: T) => contem(c1, x) || contem(c2, x)
+  def intersecao[T](c1: Conjunto[T], c2: Conjunto[T]): Conjunto[T] =
+    (x: T) => contem(c1, x) && contem(c2, x)
+  def diferenca[T](c1: Conjunto[T], c2: Conjunto[T]): Conjunto[T] =
+    (x: T) => contem(c1, x) && !contem(c2, x)
+  def filtro[T](c: Conjunto[T], f: T => Boolean): Conjunto[T] =
+    intersecao(c, f)
+  def map[T, U](c: Conjunto[T], f: U => T): Conjunto[U] =
+    (x: U) => contem(c, f(x))
 
   trait ConjInt {
-    def contem(x: Int): Boolean = ???
-    def insere(x: Int): ConjInt = ???
-    def uniao(outro: ConjInt): ConjInt = ???
+    def contem(x: Int): Boolean = this match {
+      case ConjVazio() => false
+      case ConjCons(elem, esq, dir) =>
+        if (elem == x) true
+        else if (x < elem) esq.contem(x)
+        else dir.contem(x)
+    }
+    def insere(x: Int): ConjInt =
+      this match {
+        case ConjVazio() => ConjCons(x, ConjVazio(), ConjVazio())
+        case ConjCons(elem, esq, dir) =>
+          if (x == elem) ConjCons(elem, esq, dir)
+          else if (x < elem) ConjCons(elem, esq.insere(x), dir)
+          else ConjCons(elem, esq, dir.insere(x))
+      }
+    def uniao(outro: ConjInt): ConjInt = outro match {
+      case ConjVazio() => this
+      case ConjCons(elem, esq, dir) => this.insere(elem).uniao(esq).uniao(dir)
+    }
 
-    def filter(p: Int => Boolean): ConjInt = ???
-    def map(f: Int => Int): ConjInt = ???
-    def flatMap(f: Int => ConjInt): ConjInt = ???
+    def filter(p: Int => Boolean): ConjInt = this match {
+      case ConjVazio() => this
+      case ConjCons(elem, esq, dir) =>
+        if (p(elem)) ConjCons(elem, esq.filter(p), dir.filter(p))
+        else esq.filter(p).uniao(dir.filter(p))
+    }
+
+    def withFilter(p: Int => Boolean): ConjInt = this.filter(p)
+    def map(f: Int => Int): ConjInt = this match {
+      case ConjVazio() => ConjVazio()
+      case ConjCons(elem, esq, dir) => esq.map(f).uniao(dir.map(f)).
+        insere(f(elem))
+    }
+    def flatMap(f: Int => ConjInt): ConjInt = this match {
+      case ConjVazio() => ConjVazio()
+      case ConjCons(elem, esq, dir) => esq.flatMap(f).uniao(dir.flatMap(f)).
+        uniao(f(elem))
+    }
   }
   case class ConjVazio() extends ConjInt
   case class ConjCons(elem: Int, esq: ConjInt, dir: ConjInt) extends ConjInt
 
-  def intersecao(c1: ConjInt, c2: ConjInt): ConjInt = ???
+  def intersecao(c1: ConjInt, c2: ConjInt): ConjInt = for {
+    x <- c1
+    y <- c2
+    if x == y
+  } yield x
 
 }
